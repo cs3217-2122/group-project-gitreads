bfeb9c4611a6cb8a98b7281c716bd6b0433b1a1c
Huang Weijie <h_weijie@yahoo.com>
2022-03-20 23:52:09 +0800

    fix: fix double fetching if lazy data source accessed twice in a short amount of time

--- GitReads/Utils/LazyDataSource.swift
+++ GitReads/Utils/LazyDataSource.swift
@@ -34,54 +34,52 @@ struct AnyDataFetcher<T>: DataFetcher {
 
 class LazyDataSource<T> {
 
+    // Actor to handle synchronization of the fetching
     actor Value {
-        var value: T?
+        private var _value: T?
 
-        func `set`(value: T) {
-            self.value = value
+        let fetcher: () async -> Result<T, Error>
+
+        init(fetcher: @escaping () async -> Result<T, Error>) {
+            self.fetcher = fetcher
+        }
+
+        var value: Result<T, Error> {
+            get async {
+                // if the data has been fetched, return it immediately
+                if let _value = _value {
+                    return .success(_value)
+                }
+
+                // otherwise we fetch the data. if we error, simply pass on the
+                // error to the caller and do not store the result. otherwise,
+                // we store the successful result for subsequent calls.
+                let result = await fetcher()
+                if case let .success(res) = result {
+                    _value = res
+                }
+
+                return result
+            }
         }
     }
 
-    private let fetcher: () async -> Result<T, Error>
-    private var fetchedValue: Value
+    private var valueActor: Value
 
     init<Fetcher: DataFetcher>(fetcher: Fetcher) where Fetcher.Value == T {
-        self.fetcher = fetcher.fetchValue
-        self.fetchedValue = Value()
+        self.valueActor = Value(fetcher: fetcher.fetchValue)
     }
 
     var value: Result<T, Error> {
         get async {
-            await fetchValue()
+            await valueActor.value
         }
     }
 
     func preload() {
-        let fetchValue = self.fetchValue
-
         Task {
-            _ = await fetchValue()
+            _ = await valueActor.value
         }
-    }
-
-    // TODO: may want to handle case where multiple fetches occur at
-    // roughly the same time
-    private func fetchValue() async -> Result<T, Error> {
-        // if the data has been fetched, return it immediately
-        if let data = await fetchedValue.value {
-            return .success(data)
-        }
-
-        // otherwise we fetch the data. if we error, simply pass on the
-        // error to the caller and do not store the result. otherwise,
-        // we store the successful result for subsequent calls.
-        let result = await fetcher()
-        guard case let .success(value) = result else {
-            return result
-        }
-
-        await fetchedValue.set(value: value)
-        return .success(value)
     }
 
     func map<NewValue>(_ transform: @escaping (T) -> NewValue) -> LazyDataSource<NewValue> {
