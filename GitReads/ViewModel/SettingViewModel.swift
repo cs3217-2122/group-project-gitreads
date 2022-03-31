3227a1bbdd917c3d2a5ece559d248617dfdb0a74
Huang Weijie <h_weijie@yahoo.com>
2022-03-31 13:20:30 +0800

    feat: persist user settings

--- GitReads/ViewModel/SettingViewModel.swift
+++ GitReads/ViewModel/SettingViewModel.swift
@@ -9,21 +9,43 @@ import Foundation
 import SwiftUI
 
 class SettingViewModel: ObservableObject {
+    private let fontSizeKey = "fontSizeSetting"
+    private let isScrollViewKey = "isScrollViewSetting"
+
     @Published private(set) var showSideBar = false
-    @Published var fontSize: Int
-    @Published var isScrollView = true
+    @Published var fontSize: Int {
+        didSet {
+            UserDefaults.standard.set(fontSize, forKey: fontSizeKey)
+        }
+    }
+
+    @Published var isScrollView: Bool {
+        didSet {
+            UserDefaults.standard.set(isScrollView, forKey: isScrollViewKey)
+        }
+    }
 
     let minSize = 10
     let maxSize = 30
 
     init() {
-        switch UIDevice.current.userInterfaceIdiom {
-        case .phone:
-            fontSize = 18
-        case .pad:
-            fontSize = 25
-        default:
-            fontSize = 25
+        let fontSize = UserDefaults.standard.integer(forKey: fontSizeKey)
+        if fontSize != 0 {
+            self.fontSize = fontSize
+        } else {
+            switch UIDevice.current.userInterfaceIdiom {
+            case .phone:
+                self.fontSize = 18
+            case .pad:
+                self.fontSize = 25
+            default:
+                self.fontSize = 25
+            }
+        }
+
+        self.isScrollView = true
+        if self.isKeyPresentInUserDefaults(key: isScrollViewKey) {
+            self.isScrollView = UserDefaults.standard.bool(forKey: isScrollViewKey)
         }
     }
 
@@ -55,5 +77,9 @@ class SettingViewModel: ObservableObject {
         withAnimation {
             isScrollView.toggle()
         }
+    }
+
+    private func isKeyPresentInUserDefaults(key: String) -> Bool {
+        UserDefaults.standard.object(forKey: key) != nil
     }
 }
